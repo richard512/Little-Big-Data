3076c47748735341ebab69964efdf55521e044cf
Richard <richardt512@yahoo.com>
2015-09-08 09:20:35 -0700

    useful

--- admin/index.php
+++ admin/index.php
@@ -1,2 +1,15 @@
+<style type="text/css">
+a {
+    display: block;
+    padding: 20px;
+    background-color: #00F;
+    border: 2px solid #00F;
+    font-size: 20px;
+    text-decoration: none;
+    color: #FFF;
+    margin: 10px;
+}
+</style>
+
 <a href="moadmin.php">Mongo Admin</a>
 <a href="/phpmyadmin/">MySQL Admin</a>
