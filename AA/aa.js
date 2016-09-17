2ad9a0a7449cf556d3259e9d0daaecaa74c65131
Richard <richardt512@yahoo.com>
2016-09-16 18:45:30 -0700

    added aa

---
+++ AA/aa.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var json2csv = require('json2csv');
+
+aadata = fs.readFileSync('aa-usa.json');
+fields = ['city', 'state']
+
+console.log('Loaded JSON')
+
+
+eval('aadata = '+aadata)
+
+for (i in aadata) {
+	isFirstLine = i==0 ? true : false;
+
+	meeting = aadata[i]
+	//console.log(meeting)
+	json2csv({ data: meeting, hasCSVColumnTitle: isFirstLine }, function(err, csv) {
+	  if (err) console.log(err);
+	  console.log(csv);
+	});
+}
+
+/*
+json2csv({ data: aadata, fields: fields }, function(err, csv) {
+  if (err) console.log(err);
+  console.log('line')
+  console.log(csv);
+});
+*/
+
+console.log('done')
