6a6bf9f3a85c384f89dd8c6bf40b62f684d412f2
Richard <richardt512@yahoo.com>
2017-04-19 20:11:26 -0700

    new data

---
+++ craigslist/category-stats-2016-10-18.sql
@@ -0,0 +1,109 @@
+SET @title = 'furniture'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'cars & trucks'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'general for sale'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'auto parts'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'farm & garden'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'auto wheels & tires'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'electronics'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'sporting goods'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'tools'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'motorcycles/scooters'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'household items'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'appliances'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'bicycles'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'musical instruments'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'clothing & accessories'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'recreational vehicles'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'antiques'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'business'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'baby & kid stuff'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'boats'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'collectibles'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'cell phones'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'computers'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'heavy equipment'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'trailers'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'atvs, utvs, snowmobiles'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'health and beauty'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'materials'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'video gaming'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'tickets'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'toys & games'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'arts & crafts'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'jewelry'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'motorcycle parts & accessories'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'bicycle parts'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'wanted'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'books & magazines'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'computer parts'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'boat parts & accessories'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'photo/video'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'cds / dvds / vhs'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'barter'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'garage & moving sales'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+SET @title = 'free stuff'; insert into `avgCatRanks` (cat, avgrank, avgperc) values ( @title, (select avg(rank) from catstats where title = @title), (select avg(perc) from catstats where title = @title) );
+
+update `catstats`
+join avgCatRanks on title = cat
+set weirdrank = round(avgrank - rank, 2), weirdperc = round(perc - avgperc, 2)
+
+
+/*
+SELECT * FROM `catstats`
+where 
+(
+    title = 'motorcycles/scooters'
+    or title = 'atvs, utvs, snowmobiles'
+    or title = 'boats'
+)
+and weirdperc > 2  
+ORDER BY `catstats`.`sub` ASC
+*/
+
+/*
+[
+  "furniture",
+  "cars & trucks",
+  "general for sale",
+  "auto parts",
+  "farm & garden",
+  "auto wheels & tires",
+  "electronics",
+  "sporting goods",
+  "tools",
+  "motorcycles/scooters",
+  "household items",
+  "appliances",
+  "bicycles",
+  "musical instruments",
+  "clothing & accessories",
+  "recreational vehicles",
+  "antiques",
+  "business",
+  "baby & kid stuff",
+  "boats",
+  "collectibles",
+  "cell phones",
+  "computers",
+  "heavy equipment",
+  "trailers",
+  "atvs, utvs, snowmobiles",
+  "health and beauty",
+  "materials",
+  "video gaming",
+  "tickets",
+  "toys & games",
+  "arts & crafts",
+  "jewelry",
+  "motorcycle parts & accessories",
+  "bicycle parts",
+  "wanted",
+  "books & magazines",
+  "computer parts",
+  "boat parts & accessories",
+  "photo/video",
+  "cds / dvds / vhs",
+  "barter",
+  "garage & moving sales",
+  "free stuff"
+]
